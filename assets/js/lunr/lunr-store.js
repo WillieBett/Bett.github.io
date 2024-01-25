0bc83fad74223126f553f9b91181e04923af2905
Bett <kimutaibett30@gmail.com>
2024-01-25 12:38:15 +0300

    first commit

---
+++ assets/js/lunr/lunr-store.js
@@ -0,0 +1,25 @@
+var store = [{
+        "title": "Another why this blog entry...",
+        "excerpt":"After years of enjoying the beautiful work others have done and contributed to the community it is time to give something back to all of you. The pure usage of a single blog entry has been reduced to the more specific or related topics I’m looking for and generally new...","categories": ["general"],
+        "tags": ["blog"],
+        "url": "/general/2021/10/25/why-this-blog.html",
+        "teaser": null
+      },{
+        "title": "Spring GraphQL server based on webflux - Part 1 - Intro",
+        "excerpt":"Reactive programming Reactive programming is a programming paradigm and has several implementation especially in the frontend world (e.g. VueJS, Angular, React). In the backend technologies it is not so widely used but could help for complex systems with huge amounts of parallel requests or many source data changes. For more...","categories": ["Spring-Webflux-GraphQL-Tutorial"],
+        "tags": ["java","spring","webflux","graphql","reactive"],
+        "url": "/spring-webflux-graphql-tutorial/2021/10/26/reactive-programming-with-spring-webflux-01-intro.html",
+        "teaser": null
+      },{
+        "title": "Spring GraphQL server based on webflux - Part 2 - Spring GraphQL on Webflux",
+        "excerpt":"Retro In Part 1 we introduced the general topics of reactive programming, webflux, GraphQL and R2DBC. Spring starter Let’s start with a standard Spring start setup. Project: Maven Language: Java (17) Spring: 2.6.0 (RC1) Dependencies: Lombok Spring reactive web Spring Data R2DBC H2 Database The Maven pom.xml looks now like...","categories": ["Spring-Webflux-GraphQL-Tutorial"],
+        "tags": ["java","spring","webflux","graphql","reactive"],
+        "url": "/spring-webflux-graphql-tutorial/2021/11/14/reactive-programming-with-spring-webflux-02-getting-started.html",
+        "teaser": null
+      },{
+        "title": "Spring Session Redis JSON with Keycloak - A journey with the allowlist of Jackson",
+        "excerpt":"What happened? I’m using Keycloak in many projects as the identity provider. I also use the keycloak-spring-boot adapter to integrate my springboot app with keycloak. I also use Redis as the session storage for projects that need a session storage that is shared between services. As there are special needs...","categories": ["Spring"],
+        "tags": ["java","spring","redis","keycloak","jackson"],
+        "url": "/spring/2022/05/10/how-to-deal-with-spring-redis-json-security-module.html",
+        "teaser": null
+      },]
